611729ca93763868d0dc5b38aa6e68159af9c78c
Fernando Chazarreta <fernandochaza@gmail.com>
2023-06-08 10:59:38 -0300

    feat: Add dinamic last modification date into footer

--- scripts/getDate.js
+++ scripts/getDate.js
@@ -1,4 +1,6 @@
 const date = new Date();
 const year = date.getFullYear();
-
 document.getElementById("year").textContent = year;
+
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last Modification: ${lastModified}`;
