a7465fb54047545b1c778da2c18b4a7d79527113
Fernando Chazarreta <fernandochaza@gmail.com>
2023-06-26 23:28:09 -0300

    fix: testing new push for deploy error

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,7 @@
-import { getDate } from "./getDate.js";
 import { toggleDarkMode } from "./toggleDarkMode.js";
 import { toggleMenu } from "./toggleMenu.js";
 import { displayVisits } from "./VisitsCount.js";
 
-getDate();
 toggleMenu();
 toggleDarkMode();
 displayVisits(".visits");
