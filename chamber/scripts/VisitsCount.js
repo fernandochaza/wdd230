e83361618eb2def5b6cff4e5b95261912a7d71fe
Fernando Chazarreta <fernandochaza@gmail.com>
2023-06-29 16:45:01 -0300

    feat: Add last visit message

---
+++ chamber/scripts/VisitsCount.js
@@ -0,0 +1,50 @@
+function getNumberOfVisits() {
+  const userVisits = JSON.parse(localStorage.getItem('visitsCount')) || 0;
+  return userVisits;
+};
+
+function getLastVisit() {
+  const lastVisit = JSON.parse(localStorage.getItem('lastVisit')) || 0;
+  return lastVisit;
+}
+
+function addNewVisit() {
+  // Get the number of visits from the user's local storage and add up one
+  let userVisits = getNumberOfVisits();
+  userVisits++;
+
+  // Get the current date in milliseconds
+  const now = Date.now();
+
+  localStorage.setItem('visitsCount', JSON.stringify(userVisits));
+  localStorage.setItem('lastVisit', JSON.stringify(now));
+};
+
+
+export function displayVisits(visitsPlaceholderClassName) {
+  const visitsPlaceholder = document.querySelector(visitsPlaceholderClassName);
+  if (getNumberOfVisits() == 0) {
+    visitsPlaceholder.textContent = "Welcome! This is your first visit!";
+    addNewVisit();
+  }
+  else {
+    // Get the current date in milliseconds
+    const now = Date.now();
+    
+    // Get the last visit time in Milliseconds
+    const lastVisit = getLastVisit();
+
+    // Calculate milliseconds since the last visit and pass it to days
+    const msToDays = 84600000;
+    const elapsedTime = (now - lastVisit) / msToDays;
+  
+    if (elapsedTime > 1) {
+      visitsPlaceholder.textContent = `You last visited ${Math.round(elapsedTime)} days ago.`;
+    }
+    else {
+      visitsPlaceholder.textContent = "Back so soon! Awesome!";
+    }
+
+    addNewVisit();
+  }
+};
--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,5 +1,7 @@
 import { getLastModification } from "./getLastModification.js";
 import { toggleMenu } from "./toggleMenu.js";
+import { displayVisits } from "./VisitsCount.js";
 
 getLastModification();
 toggleMenu();
+displayVisits(".visits");
