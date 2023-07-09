2472b78241a22324b748e32ae60cc44b23c49d80
Fernando Chazarreta <fernandochaza@gmail.com>
2023-07-08 21:00:50 -0300

    feat: week05 weather info

---
+++ scripts/displayWeather.js
@@ -0,0 +1,39 @@
+export function displayWeather() {
+  const weatherInfo = document.querySelector("#weather-info");
+  const weatherIcon = document.querySelector("#weather-icon");
+
+  const key = "fb29c431fd773dbb2b992a9ede88900a";
+  const lat = "-46.44";
+  const lon = "-67.52";
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${key}`;
+
+  async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.table(data);
+        displayResults(data);
+      } else {
+        throw new Error(await response.text())
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const displayResults = (weatherEvent) => {
+
+    const eventDescription = weatherEvent.weather[0].description;
+    const iconUrl = `https://openweathermap.org/img/w/${weatherEvent.weather[0].icon}.png`;
+    const temp = `${weatherEvent.main.temp}°C`;
+
+    weatherIcon.setAttribute('src', iconUrl);
+    weatherIcon.setAttribute('alt', `${eventDescription} Icon`);
+
+    weatherInfo.textContent += `${temp} - ${eventDescription}`;
+  }
+
+  apiFetch()
+}
--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,10 @@ import { getDate } from "./getDate.js";
 import { toggleDarkMode } from "./toggleDarkMode.js";
 import { toggleMenu } from "./toggleMenu.js";
 import { displayVisits } from "./visitsCount.js"
+import { displayWeather } from './displayWeather.js';
 
 getDate();
 toggleMenu();
 toggleDarkMode();
 displayVisits(".visits");
+displayWeather();
